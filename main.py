3f15c8316d132dff5622d40e7a0e776d5e58e9f0
trpubz <taylor.pubins@me.com>
2022-06-01 13:26:16 -0700

    updated resources

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-# FBB_Positional_Analyzer
+# TRP_Positional_Analyzer
 # main.py
-# This script imports
+# This script runs the module
 # by pubins.taylor
-# v0.1 - 23 MAY 2022
+# v0.1 - 30 MAY 2022
 
+import pandas as pd
+import matplotlib
 
-def print_hi(name):
-    print(f'Hi, {name}')
+bats = ["OF", "1B", "3B", "2B", "SS", "DH", "C"]
+arms = ["SP", "RP"]
+hitters = pd.read_json("resources/TRPHitterCards.json")
+pitchers = pd.read_json("resources/TRPPitcherCards.json")
+
+def TRPFilterHitters(pos: str):
+    posPlayers = hitters[hitters["pos"].str.contains(pos)]
+    print(f"POS: {pos}")
+    print(posPlayers)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    for p in bats:
+        TRPFilterHitters(pos=p)
+
 
