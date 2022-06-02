728888c3337df74210cd3247b368dce746c3c1ee
trpubz <taylor.pubins@me.com>
2022-06-01 21:20:10 -0700

    plots and data sets finalized

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 # TRP_Positional_Analyzer
+# TRP is a custom Fantasy Baseball projection data set that stands for Truncated Runs Produced.  It relies on another's
+# projection system (generally a product from Fangraphs) which focuses on a derived wRAA (weighted Runs Above Avg)
+# and current expected stats from Baseball Savant.  The combined projection & current performance metrics produce TRP.
+# This Positional Analyzer takes the TRP Datasets called player 'Cards' stored in the 'resources' folder and generates
+# graphs and reports to make quick-informed decisions.  All functions specific to this project & not inside a class are
+# designated with the 'TRP' namespace prefix.
 # main.py
-# This script runs the module
 # by pubins.taylor
 # v0.5 - 1 JUN 2022
 
 import pandas as pd
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+from statistics import mean
 
 # instantiate the global objects
 bats = ["OF", "1B", "3B", "2B", "SS", "DH", "C"]  # these strings represent the offensive positions
@@ -25,19 +30,79 @@ pitchers = pitchers.reindex(columns=["_name", "tm", "pos", "fantasyTeam",
                                      "IP", "QS", "SVHD", "K/9", "ERA", "WHIP"])
 
 
-def TRPFilterPosGroup(pos: str):
+def TRPFilterPosGroup(pos: str) -> (str, pd.DataFrame):
+    """
+    Function that filters the Position Group
+    :parameter pos: a string representation of the roster slot
+    :returns: tuple with the pos: str and the posGroup DataFrame
+    """
     if pos.__contains__("P"):
         posPlayers = pitchers[pitchers["pos"].str.contains(pos)]
+        # the dataset is sorted on xERA so the top skill is already at the top of the list
+        if pos == "SP":
+            topPlayers = posPlayers.head(
+                12 * 5 + 26)  # 12 (num of teams) * 5 (potential SP slots) + 16 (arbitrary overage buffer)
+        else:
+            topPlayers = posPlayers.head(
+                12 * 4 + 26)  # 12 (num of teams) * 4 (potential RP slots) + 16 (arbitrary overage buffer)
     else:
         posPlayers = hitters[hitters["pos"].str.contains(pos)]
+        # trim the excess number of players for analysis
+        # the dataset is sorted on wRAA so the top skill is already at the top of the list
+        if pos == "OF":
+            topPlayers = posPlayers.head(60)  # 3x more OF than any other position so get 3x results
+        else:
+            topPlayers = posPlayers.head(20)
 
     print(f"POS: {pos}")
-    print(posPlayers.to_string())
+    print(topPlayers.to_string())
+    return pos, topPlayers
+
+
+def TRPScatterPlotBuilder(pos: str, data: pd.DataFrame):
+    xCat: str
+    yCat: str
+    sCat: str  # size category
+    if pos.__contains__("P"):
+        xCat = "WHIP"
+        yCat = "ERA"
+        sCat = "xERA"
+    else:
+        xCat = "OBP"
+        yCat = "SLG"
+        sCat = "wRAA"
+
+    # extract the data
+    x = data[xCat].to_numpy()
+    y = data[yCat].to_numpy()
+    # size and color based on wRAA:
+    sizes = data[sCat].apply(lambda r: r * 10)
+    colors = data[sCat].apply(lambda r: r * 10)
+
+    # plot
+    fig, ax = plt.subplots()
+
+    ax.scatter(x, y, s=sizes, c=colors, vmin=0, vmax=100, alpha=0.5)
+    # We only want to place the scatter plot labels only for those players that are Free Agents
+    for name in data["_name"]:
+        if data.fantasyTeam[data["_name"] == name].values[0] == "FA":  # 1st index value holds the string rep
+            plt.text(x=data[xCat][data["_name"] == name], y=data[yCat][data["_name"] == name], s=name)
+    ax.set_xlabel(xCat)
+    ax.set_ylabel(yCat)
+    ax.set_title(f'POS: {pos}')
+    plt.axvline(data[xCat].mean(), c='black', ls='-')
+    plt.axhline(data[yCat].mean(), c='black', ls='-')
+    plt.grid()
+    plt.figure(figsize=(18, 12))
+
+    plt.show()
 
 
 if __name__ == '__main__':
     for player in bats:
-        TRPFilterPosGroup(pos=player)
+        posGroup, dfPos = TRPFilterPosGroup(pos=player)
+        TRPScatterPlotBuilder(pos=posGroup, data=dfPos)
 
     for player in arms:
-        TRPFilterPosGroup(pos=player)
+        posGroup, dfPos = TRPFilterPosGroup(pos=player)
+        TRPScatterPlotBuilder(pos=posGroup, data=dfPos)
--- sources/Export/report_generator_example.py
+++ sources/Export/report_generator_example.py
@@ -1,16 +1,17 @@
 import report_util
 import numpy as np
 
+
 def generate_report(dataset):
     report = report_util.Report("Random Number Dataset Report")
 
     section = report.add_section("Dataset Stats")
 
     paragraph = section.add_paragraph()
-    mean = sum(dataset)/len(dataset)
+    mean = sum(dataset) / len(dataset)
     standard_deviation = dataset.std()
-    
-    paragraph.append(f"The dataset has {len(dataset)} numbers. ") 
+
+    paragraph.append(f"The dataset has {len(dataset)} numbers. ")
     paragraph.append(f"The largest number in the dataset is {max(dataset)}. ")
     paragraph.append(f"The smallest number in the dataset is {min(dataset)}. ")
     paragraph.append(f"The dataset average value is {mean} with a standard deviation of {standard_deviation}. ")
@@ -22,7 +23,7 @@ def generate_report(dataset):
     figure_1 = section.add_figure()
     figure_1.caption = "Dataset Histogram"
     # notice in the next line we access matplotlib's figure object directly
-    ax = figure_1.matplotlib_figure.add_subplot(1,1,1)
+    ax = figure_1.matplotlib_figure.add_subplot(1, 1, 1)
     ax.hist(dataset)
     ax.set_xlabel("Number Values")
     ax.set_ylabel("Count")
@@ -38,12 +39,11 @@ def generate_report(dataset):
     tbl_1 = section.add_table()
     tbl_1.caption = "Dataset Listing"
 
-    tbl_1.set_header(["Value","Less Than 0.3","Has digit '3'"])
-    tbl_1.set_data(zip(dataset,[x<0.3 for x in dataset],[str(x).find('3') > -1 for x in dataset]))
+    tbl_1.set_header(["Value", "Less Than 0.3", "Has digit '3'"])
+    tbl_1.set_data(zip(dataset, [x < 0.3 for x in dataset], [str(x).find('3') > -1 for x in dataset]))
 
     paragraph_2.append_cross_reference(tbl_1)
     paragraph_2.append(f" shows the numbers in the dataset with some other properties of these numbers. ")
-
 
     ##########################################################################
     # The following code demonstrates creating another section to the report
@@ -55,18 +55,18 @@ def generate_report(dataset):
     # The following code demonstrates creating a figure directly with the matplotlib API
     ##########################################################################
     figure_2 = section_2.add_figure()
-    
+
     # Generate some more example data for figure
     dt = 0.01
     t = np.arange(0, 30, dt)
-    nse1 = np.random.randn(len(t))                 # white noise 1
-    nse2 = np.random.randn(len(t))                 # white noise 2
+    nse1 = np.random.randn(len(t))  # white noise 1
+    nse2 = np.random.randn(len(t))  # white noise 2
     s1 = np.sin(2 * np.pi * 10 * t) + nse1
     s2 = np.sin(2 * np.pi * 10 * t) + nse2
 
     # The following starts the plot creation using data generated above
     # notice in the next line we access matplotlib's figure object directly
-    axs = [figure_2.matplotlib_figure.add_subplot(2, 1, 1),figure_2.matplotlib_figure.add_subplot(2, 1, 2)]
+    axs = [figure_2.matplotlib_figure.add_subplot(2, 1, 1), figure_2.matplotlib_figure.add_subplot(2, 1, 2)]
     axs[0].plot(t, s1, t, s2)
     axs[0].set_xlim(0, 2)
     axs[0].set_xlabel('time')
@@ -80,7 +80,7 @@ def generate_report(dataset):
 
     paragraph_3.append_cross_reference(figure_2)
     paragraph_3.append(" shows a plot of more random numbers.")
-    n_mean = sum(nse1)/len(nse1)
+    n_mean = sum(nse1) / len(nse1)
     if n_mean > mean:
         paragraph_3.append("This dataset has a larger mean compared to the baseline dataset. ")
     else:
@@ -89,10 +89,11 @@ def generate_report(dataset):
 
     return report
 
+
 if __name__ == "__main__":
-    #np.random.seed(19680801)
+    # np.random.seed(19680801)
     dataset = np.random.randn(50)
     report = generate_report(dataset)
 
     html_generator = report_util.HTMLReportContext("")
-    html_generator.generate(report,"example")
+    html_generator.generate(report, "example")
